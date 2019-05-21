ad83680ee5a9ae82f3305800247a539bbe499eb9
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-05-20 20:49:24 -0500

    Connecting to the database

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,31 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) :base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        //Could have omitted the above two and it would work the same.
+        //The Entity Framework would include them implicitly because the 
+        //Student entity references the Enrollment entity and the Enrollment 
+        //entity references the Course entity.
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+        }
+    }
+}
