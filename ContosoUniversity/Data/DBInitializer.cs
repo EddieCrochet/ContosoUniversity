8dba2b7333209c8de84dd55c940002bc54910fe3
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-05-22 22:21:56 -0500

    modify main in program class to use the initialize seed method

--- ContosoUniversity/Data/DBInitializer.cs
+++ ContosoUniversity/Data/DBInitializer.cs
@@ -12,7 +12,7 @@ namespace ContosoUniversity.Data
         {
             context.Database.EnsureCreated();
 
-            // Look for any students.
+            // Look for any students in the database.
             if (context.Students.Any())
             {
                 return;   // DB has been seeded
