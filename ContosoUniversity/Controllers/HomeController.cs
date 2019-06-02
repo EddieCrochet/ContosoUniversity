6246e0c5a23278a0cb5a9dc724aed5fe56afedb5
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-06-01 20:43:02 -0500

    added the method in my home controller that with sort the students and group by enrollment date

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -5,11 +5,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContosoUniversity.Models;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            context = _context;
+        }
+
+        //I wanted to keep my about page so from the contoso uni 
+        //app tutorial this is what they called about
+        public async Task<IActionResult> StudentsPerDate()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                //store in a collection of EnrollmentDateGroup view model objects
+                from student in _context.Students
+                //groups student entities by enrollment date
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    //calculates number of entities in each group
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
         public IActionResult Index()
         {
             return View();
